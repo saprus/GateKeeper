67d54bd12d7b392254498087e51e6c971323e407
Siddhant Sapru <siddhantsapru@Siddhants-MacBook-Pro.local>
2024-09-08 16:15:17 -0400

    Adding basic files

---
+++ GateKeeper/GateKeeper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GateKeeper
+//
+//  Created by Siddhant Sapru on 2024-09-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, my world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GateKeeper/GateKeeper/GateKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  GateKeeperApp.swift
+//  GateKeeper
+//
+//  Created by Siddhant Sapru on 2024-09-08.
+//
+
+import SwiftUI
+
+@main
+struct GateKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
