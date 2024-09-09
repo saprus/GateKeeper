0571cd9d39b76756dee67cdb5d86e466775489a5
Siddhant Sapru <siddhantsapru@Siddhants-MacBook-Pro.local>
2024-09-08 21:20:32 -0400

    Added firebase config

--- GateKeeper/GateKeeper/GateKeeperApp.swift
+++ GateKeeper/GateKeeper/GateKeeperApp.swift
@@ -6,9 +6,26 @@
 //
 
 import SwiftUI
+import Firebase
+import UserNotifications
 
 @main
 struct GateKeeperApp: App {
+    
+    // Create an instance of the AppDelegate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    //Initialize FireBase when the app starts
+    init() {
+        FirebaseApp.configure()
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) {
+            granted, error in print("Permssion granted: \(granted)")
+        }
+        
+        UIApplication.shared.registerForRemoteNotifications()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
---
+++ GateKeeper/Pods/Target Support Files/Pods-GateKeeper/Pods-GateKeeper-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GateKeeper : NSObject
+@end
+@implementation PodsDummy_Pods_GateKeeper
+@end
